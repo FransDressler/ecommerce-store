e816d62186bd2e997a9031669fc68661bd121587
Frans Dressler <frans@Franss-MacBook-Air.local>
2023-08-15 16:08:18 +0200

    Set up style sheet

---
+++ pages/home.tsx
@@ -0,0 +1,11 @@
+import '../styles/styles.scss'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+
+export default function Home() {
+  return (
+          <section id="header">
+            <p className="">Hallo</p>
+          </section>
+      )
+}
